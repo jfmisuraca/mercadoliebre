ada800ccc0565af9ed5b7e8eae09a9dc1a1f9f17
Juan Francisco <jfmisuraca@hotmail.com>
2023-07-27 18:49:16 -0300

    modificados estilos

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const path = require('path')
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
-  res.send('Hola mundo!');
+  res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
 app.listen(3000, () => {
   console.log('Servidor corriendo')
 });
 
-app.use(express.static)
+// app.use(express.static)
